50569906676b238259a1433ccbfcf09b2e819f44
Honza Dvorsky <honza@swiftkey.net>
2014-03-22 11:42:45 +0000

    NSDictionary enumeration can look nicer with block-based enumeration.

--- RPJSONValidator/RPJSONValidator.m
+++ RPJSONValidator/RPJSONValidator.m
@@ -29,64 +29,64 @@ static BOOL RPJSONValidatorShouldSuppressWarnings;
     // * Values: RPValidatorPredicate/NSDictionary
     //
     // The error parameter may be nil. Otherwise, it must be an NSError class
-
+    
     if(!json) {
         [RPJSONValidator log:@"RPJSONValidator Error: json parameter is nil -- there is nothing to validate. Returning NO"];
         if(error) {
             *error = [NSError errorWithDomain:RPJSONValidatorErrorDomain
                                          code:RPJSONValidatorErrorBadJSONParameter
                                      userInfo:@{
-                                             NSLocalizedDescriptionKey : @"Nothing to validate",
-                                             NSLocalizedFailureReasonErrorKey : @"json parameter is nil",
-                                             NSLocalizedRecoverySuggestionErrorKey : @"pass in valid json"
-                                     }];
+                                                NSLocalizedDescriptionKey : @"Nothing to validate",
+                                                NSLocalizedFailureReasonErrorKey : @"json parameter is nil",
+                                                NSLocalizedRecoverySuggestionErrorKey : @"pass in valid json"
+                                                }];
         }
-
+        
         return NO;
     }
-
+    
     if(![json isKindOfClass:[NSDictionary class]] && ![json isKindOfClass:[NSArray class]]) {
         [RPJSONValidator log:@"RPJSONValidator Error: json parameter is not valid JSON (it is not an NSArray or NSDictionary). Returning NO"];
         if(error) {
             *error = [NSError errorWithDomain:RPJSONValidatorErrorDomain
                                          code:RPJSONValidatorErrorBadJSONParameter
                                      userInfo:@{
-                                             NSLocalizedDescriptionKey : @"Nothing to validate",
-                                             NSLocalizedFailureReasonErrorKey : @"json parameter is not an NSArray or NSDictionary",
-                                             NSLocalizedRecoverySuggestionErrorKey : @"pass in valid json"
-                                     }];
+                                                NSLocalizedDescriptionKey : @"Nothing to validate",
+                                                NSLocalizedFailureReasonErrorKey : @"json parameter is not an NSArray or NSDictionary",
+                                                NSLocalizedRecoverySuggestionErrorKey : @"pass in valid json"
+                                                }];
         }
         return NO;
     }
-
+    
     if(!requirements) {
         [RPJSONValidator log:@"RPJSONValidator Error: requirements parameter is nil -- there are no requirements. Returning NO"];
         if(error) {
             *error = [NSError errorWithDomain:RPJSONValidatorErrorDomain
                                          code:RPJSONValidatorErrorBadRequirementsParameter
                                      userInfo:@{
-                                             NSLocalizedDescriptionKey : @"Nothing to validate",
-                                             NSLocalizedFailureReasonErrorKey : @"requirements parameter is nil",
-                                             NSLocalizedRecoverySuggestionErrorKey : @"pass in valid requirements"
-                                     }];
+                                                NSLocalizedDescriptionKey : @"Nothing to validate",
+                                                NSLocalizedFailureReasonErrorKey : @"requirements parameter is nil",
+                                                NSLocalizedRecoverySuggestionErrorKey : @"pass in valid requirements"
+                                                }];
         }
         return NO;
     }
-
+    
     if(![requirements isKindOfClass:[NSDictionary class]]) {
         [RPJSONValidator log:@"RPJSONValidator Error: requirements parameter is not an NSDictionary. Returning NO"];
         if(error) {
             *error = [NSError errorWithDomain:RPJSONValidatorErrorDomain
                                          code:RPJSONValidatorErrorBadRequirementsParameter
                                      userInfo:@{
-                                             NSLocalizedDescriptionKey : @"Nothing to validate",
-                                             NSLocalizedFailureReasonErrorKey : @"requirements parameter is not an NSDictionary",
-                                             NSLocalizedRecoverySuggestionErrorKey : @"pass in valid requirements"
-                                     }];
+                                                NSLocalizedDescriptionKey : @"Nothing to validate",
+                                                NSLocalizedFailureReasonErrorKey : @"requirements parameter is not an NSDictionary",
+                                                NSLocalizedRecoverySuggestionErrorKey : @"pass in valid requirements"
+                                                }];
         }
         return NO;
     }
-
+    
     return [RPJSONValidator validateValuesFrom:json
                               withRequirements:requirements
                                          error:error
@@ -104,11 +104,11 @@ static BOOL RPJSONValidatorShouldSuppressWarnings;
     // * Keys: NSString/NSNumber
     //      * Keys correspond to JSON keys or indices of arrays
     // * Values: RPValidatorPredicate/NSDictionary
-
-    for(id requirementsKey in [requirements allKeys]) {
-        id requirementsValue = [requirements objectForKey:requirementsKey];
+    
+    [requirements enumerateKeysAndObjectsUsingBlock:^(id requirementsKey, id requirementsValue, BOOL *stop) {
+        
         id jsonValue;
-
+        
         if([json isKindOfClass:[NSArray class]] && [requirementsKey isKindOfClass:[NSNumber class]] && [json count] > [requirementsKey unsignedIntegerValue]) {
             jsonValue = [json objectAtIndex:[requirementsKey unsignedIntegerValue]];
         } else if([json isKindOfClass:[NSDictionary class]]) {
@@ -116,16 +116,16 @@ static BOOL RPJSONValidatorShouldSuppressWarnings;
         } else {
             jsonValue = nil;
         }
-
+        
         if([requirementsValue isKindOfClass:[RPValidatorPredicate class]] && [requirementsKey isKindOfClass:[NSString class]]) {
             [(RPValidatorPredicate *)requirementsValue validateValue:jsonValue withKey:requirementsKey];
-
+            
             if([[(RPValidatorPredicate *)requirementsValue failedRequirementDescriptions] count]) {
                 NSMutableDictionary *failingKeys = [userInfo objectForKey:RPJSONValidatorFailingKeys];
-
+                
                 if(failingKeys) failingKeys = [failingKeys mutableCopy];
                 else failingKeys = [NSMutableDictionary dictionary];
-
+                
                 [failingKeys setObject:[(RPValidatorPredicate *)requirementsValue failedRequirementDescriptions] forKey:requirementsKey];
                 [userInfo setObject:failingKeys forKey:RPJSONValidatorFailingKeys];
             }
@@ -139,19 +139,24 @@ static BOOL RPJSONValidatorShouldSuppressWarnings;
             *error = [NSError errorWithDomain:RPJSONValidatorErrorDomain
                                          code:RPJSONValidatorErrorBadRequirementsParameter
                                      userInfo:@{
-                                             NSLocalizedDescriptionKey : @"Requirements not setup correctly",
-                                             NSLocalizedFailureReasonErrorKey : [NSString stringWithFormat:@"Requirements key (%@) with value (%@) is not an RPValidatorPredicate or NSDictionary", requirementsKey, requirementsValue],
-                                             NSLocalizedRecoverySuggestionErrorKey : @"Review requirements syntax"
-                                     }];
-            return NO;
+                                                NSLocalizedDescriptionKey : @"Requirements not setup correctly",
+                                                NSLocalizedFailureReasonErrorKey : [NSString stringWithFormat:@"Requirements key (%@) with value (%@) is not an RPValidatorPredicate or NSDictionary", requirementsKey, requirementsValue],
+                                                NSLocalizedRecoverySuggestionErrorKey : @"Review requirements syntax"
+                                                }];
+            *stop = YES;
         }
+        
+    }];
+    
+    if (*error) {
+        return NO;
     }
-
+    
     if([[userInfo allKeys] count] && error) {
         [userInfo setObject:@"JSON did not validate" forKey:NSLocalizedDescriptionKey];
         [userInfo setObject:@"At least one requirement wasn't met" forKey:NSLocalizedFailureReasonErrorKey];
         [userInfo setObject:@"Perhaps use backup JSON" forKey:NSLocalizedRecoverySuggestionErrorKey];
-
+        
         *error = [NSError errorWithDomain:RPJSONValidatorErrorDomain code:RPJSONValidatorErrorInvalidJSON userInfo:userInfo];
         return NO;
     }
@@ -159,16 +164,18 @@ static BOOL RPJSONValidatorShouldSuppressWarnings;
 }
 
 + (NSString *)prettyStringGivenRPJSONValidatorError:(NSError *)error {
-    NSString *prettyString = @"";
-
-    for(NSString *badKey in [[[error userInfo] objectForKey:RPJSONValidatorFailingKeys] allKeys]) {
-        NSArray *requirements = [[[error userInfo] objectForKey:RPJSONValidatorFailingKeys] objectForKey:badKey];
-
+    __block NSString *prettyString = @"";
+    
+    NSDictionary *failingKeys = [[error userInfo] objectForKey:RPJSONValidatorFailingKeys];
+    
+    [failingKeys enumerateKeysAndObjectsUsingBlock:^(NSString *badKey, NSArray *requirements, BOOL *stop) {
         prettyString = [prettyString stringByAppendingFormat:@"* %@\n", badKey];
-        for(NSString *requirement in requirements)
+        
+        [requirements enumerateObjectsUsingBlock:^(NSString *requirement, NSUInteger idx, BOOL *stop_in) {
             prettyString = [prettyString stringByAppendingFormat:@"     * %@\n", requirement];
-    }
-
+        }];
+    }];
+    
     return prettyString;
 }
 
